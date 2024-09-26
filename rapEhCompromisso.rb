191d8b5645d97f2a81f65e2f6b1700e56db760d1
Anderson Fér <andersonfer@gmail.com>
2024-09-26 13:58:10 -0700

    linha de baixo

---
+++ rapEhCompromisso.rb
@@ -0,0 +1,33 @@
+use_bpm 90
+
+live_loop :bass do
+  use_synth :piano
+  play :a2
+  sleep 2
+  play :a2
+  sleep 0.5
+  play :a2
+  sleep 1
+  play :g2
+  sleep 0.5
+  play :a2
+  sleep 2
+  play :a2
+  sleep 1
+  play :g2
+  sleep 1
+  play :f2
+  sleep 2
+  play :f2
+  sleep 0.5
+  play :f2
+  sleep 1
+  play :e2
+  sleep 0.5
+  play :f2
+  sleep 2
+  play :f2
+  sleep 1
+  play :g2
+  sleep 1
+end
